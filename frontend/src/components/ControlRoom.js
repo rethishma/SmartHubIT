a7a8035b03fae5baa11c645f1c112b873769652f
rethishma <24eg105v18@anurag.edu.in>
2026-03-31 21:30:42 +0530

    Create ControlRoom.js

---
+++ frontend/src/components/ControlRoom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ControlRoom() {
+  return (
+    <div>
+      <h2>Control Room Dashboard</h2>
+      <p>Monitor all flats, trigger alerts, analytics</p>
+    </div>
+  );
+}
+
+export default ControlRoom;
