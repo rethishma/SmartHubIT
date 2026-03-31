66b4a84b0f5798aa5cfd4bbdf49a715b2a91399d
rethishma <24eg105v18@anurag.edu.in>
2026-03-31 21:30:13 +0530

    Create Dashboard.js

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard({ flat }) {
+  return (
+    <div>
+      <h2>Flat Dashboard</h2>
+      <p>Welcome Flat {flat}</p>
+      <p>Temperature, Lights, Alerts UI here</p>
+    </div>
+  );
+}
+
+export default Dashboard;
